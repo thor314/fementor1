e56540d1587b8adb93c84bf89f6b75cb03b79a5f
Thor Kamphefner <thorck@pm.me>
2023-02-22 13:05:50 -0800

    pre-background

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,31 @@ function App() {
   return (
     <div className="App">
       <main>
-        You’ve used 815 GB of your storage 
-        
-        185 GB Left 
-
-        0 GB 
-
-        1000 GB
-        <div className="attribution">
-          Challenge by{" "}
-          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
-            Frontend Mentor
-          </a>
-          . Coded by <a href="#">Your Name Here</a>.
+        <div className="top-header">
+          {/* todo icon */}
+          <h1>Fylo</h1>
+          <ul className="header-icons">
+            {/* todo icons */}
+            <li>*</li>
+            <li>*</li>
+            <li>*</li>
+          </ul>
+        </div>
+        <div className="top-storage">
+          You’ve used 815 GB of your storage 185 GB Left 0 GB 1000 GB
+          <div className="attribution">
+            Challenge by{" "}
+            <a
+              href="https://www.frontendmentor.io?ref=challenge"
+              target="_blank"
+            >
+              Frontend Mentor
+            </a>
+            . Coded by <a href="#">Your Name Here</a>.
+          </div>
+          <div className="top-storage-left">
+            185
+          </div>
         </div>
       </main>
     </div>
